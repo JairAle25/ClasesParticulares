5b5e0ce5f291cb3f838262c541606196d3846eb5
Jair Cornejo <jaircornejo61@gmail.com>
2023-09-26 10:42:13 -0300

    mensaje de confirmacion al tocar el boton

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const botonCvu = document.getElementById('cvu');
 botonCvu.addEventListener('click', function(event) {
     navigator.clipboard.writeText('Jair.Cornejo.25')
     .then(() => {
-      console.log('Texto copiado al portapapeles');
+      Swal.fire({
+        position: 'bottom-center',
+        icon: 'success',
+        title: 'CVU copiado correctamente',
+        showConfirmButton: false,
+        timer: 1300
+      })
     })
     .catch(err => {
-      console.error('Error al copiar el texto: ', err);
+      Swal.fire({
+        position: 'bottom-center',
+        icon: 'error',
+        title: 'Error al copiar el CVU',
+        showConfirmButton: false,
+        timer: 1300
+      })
+      console.error(err);
     });
   });
